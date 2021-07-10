fb604904ea2f42852c4bf0c39200e2f54cf0245e
Maksym Voloshko <anivone18052001@gmail.com>
2021-07-11 00:48:49 +0300

    [Client]: Client application initialized

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
